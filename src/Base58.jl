009ea5c2e86cbf7d073e4524d01d9961dc7cd167
Guido Kraemer <gdkrmr@users.noreply.github.com>
2018-03-09 22:10:46 +0100

    base58checkencoding works

--- src/Base58.jl
+++ src/Base58.jl
@@ -148,65 +148,28 @@ function base58decode(x::T) where T <: Union{Array{UInt8, 1},
     end
 end
 
-
-# Bitcoin specific constants
-
-# Table from https://en.bitcoin.it/wiki/List_of_address_prefixes
-
-# Decimal prefix Hex       Example use                                     Leading Symbol(s) Example
-# -----------------------------------------------------------------------------------------------------------
-# 0              00        Pubkey hash (P2PKH address)                     1                 17VZNX1SN5NtKa8UQFxwQbFeFc3iqRYhem
-# 5              05        Script hash (P2SH address)                      3                 3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX
-# 128            80        Private key (WIF, uncompressed pubkey)          5                 5Hwgr3u458GLafKBgxtssHSPqJnYoGrSzgQsPwLFhLNYskDPyyA
-# 128            80        Private key (WIF, compressed pubkey)            K or L            L1aW4aubDFB7yfras2S1mN3bqg9nwySY8nkoLmJebSLD5BWv3ENZ
-# 4 136 178 30   0488B21E  BIP32 pubkey                                    xpub              xpub661MyMwAqRbcEYS8w7XLSVeEsBXy79zSzH1J8vCdxAZningWLdN3zgtU6LBpB85b3D2yc8sfvZU521AAwdZafEz7mnzBBsz4wKY5e4cp9LB
-# 4 136 173 228  0488ADE4  BIP32 private key                               xprv              xprv9s21ZrQH143K24Mfq5zL5MhWK9hUhhGbd45hLXo2Pq2oqzMMo63oStZzF93Y5wvzdUayhgkkFoicQZcP3y52uPPxFnfoLZB21Teqt1VvEHx
-# 111            6F        Testnet pubkey hash                             m or n            mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn
-# 196            C4        Testnet script hash                             2                 2MzQwSSnBHWHqSAqtTVQ6v47XtaisrJa1Vc
-# 239            EF        Testnet Private key (WIF, uncompressed pubkey)  9                 92Pg46rUhgTT7romnV7iGW6W1gbGdeezqdbJCzShkCsYNzyyNcc
-# 239            EF        Testnet Private key (WIF, compressed pubkey)    c                 cNJFgo1driFnPcBdBX8BrJrpxchBWXwXCvNH5SoSkdcF6JXXwHMm
-# 4 53 135 207   043587CF  Testnet BIP32 pubkey                            tpub              tpubD6NzVbkrYhZ4WLczPJWReQycCJdd6YVWXubbVUFnJ5KgU5MDQrD998ZJLNGbhd2pq7ZtDiPYTfJ7iBenLVQpYgSQqPjUsQeJXH8VQ8xA67D
-# 4 53 131 148   04358394  Testnet BIP32 private key                       tprv              tprv8ZgxMBicQKsPcsbCVeqqF1KVdH7gwDJbxbzpCxDUsoXHdb6SnTPYxdwSAKDC6KKJzv7khnNWRAJQsRA8BBQyiSfYnRt6zuu4vZQGKjeW4YF 
-
-const P2PKSH  = (0x00, )
-const P2SH    = (0x05, )
-const WIF     = (0x80, )
-const XPUB    = (0x04, 0x88, 0xb2, 0x1e)
-const XPRIV   = (0x04, 0x88, 0xad, 0xe4)
-const TP2PKSH = (0x6f, )
-const TP2SH   = (0xc4, )
-const TWIF    = (0xef, )
-const TPUB    = (0x04, 0x35, 0x87, 0xcf)
-const TPRIV   = (0x04, 0x35, 0x83, 0x94)
-
-function base58checkencode(x::T, prefix::U = P2PKSH) where
-    T <: Union{Array{UInt8, 1}, NTuple{N, UInt8} where N} where
-    U <: Union{Array{UInt8, 1}, NTuple{N, UInt8} where N}
-
-    base58encode([
-        prefix..., x..., sha256(sha256([prefix..., x...]))[1:4]...
-    ])
-end
-
-function base58checkdecode(x::T, check = true) where
+function base58checkencode(payload::T) where
     T <: Union{Array{UInt8, 1}, NTuple{N, UInt8} where N}
 
-    res = base58decode(x[1:end - 4])
+    checksum = sha256(sha256(payload))[1:4]
+
+    base58encode([payload..., checksum...])
+end
+
+function base58checkdecode(x::T, check::Bool = true) where
+    T <: Union{Array{UInt8, 1}, NTuple{N, UInt8} where N}
+
+    dec = base58decode(x)
+    payload = dec[1:end-4]
+    checksum = dec[end-3:end]
 
     if check
-        if sha256(sha256(res[1:end - 4]))[1:4] != res[end - 3:end]
+        if sha256(sha256(payload))[1:4] != checksum
             throw(ArgumentError("Invalid address"))
         end
     end
 
-    return res
+    return payload
 end
-
-# base58decode(x::Base58) = base58decode(x.data)
-
-# function Base58Check(x::T) where T<:Union{Array{UInt8, 1}, NTuple{N, UInt8} where N}
-#     checksum = SHA.sha256(SHA.sha256(x))
-#     Base58Check(Base58(x).string * checksum[end-3:end])
-# end
 
 end # module Base58
